628978433fc1a4af929b815c3b3965067189f2b2
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-07-28 14:27:30 -0300

    exercicio 04 Consistência

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,54 @@
+
+
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        this.saudeAtual = saudeAtual;
+        this.nome = nome;
+        setSaudeAtual(saudeAtual);
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if(saudeAtual > 0){
+            status="vivo";
+        } else if(saudeAtual == 0){
+            status = "morto";
+        }
+    }
+
+    public void setNome(String nome) {
+        if(nome != null && !nome.isEmpty()) {
+            this.nome = nome;
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void tomarDano(int quantidadeDeDano){
+        setSaudeAtual(getSaudeAtual()-quantidadeDeDano);
+        if(getSaudeAtual()<0){
+            setSaudeAtual(0);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura){
+       setSaudeAtual(getSaudeAtual()+quantidadeDeCura);
+        if(getSaudeAtual()>= 100) {
+            setSaudeAtual(100);
+        }
+    }
+}
