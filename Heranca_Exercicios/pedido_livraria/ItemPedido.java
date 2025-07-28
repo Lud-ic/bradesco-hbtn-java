d72e9454fd72dccd1a3de94789ab2eae82e056a4
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-07-28 20:48:52 -0300

    exercício 0. Pedido Livraria ajuste import e construtor Item pedido

--- Heranca_Exercicios/pedido_livraria/ItemPedido.java
+++ Heranca_Exercicios/pedido_livraria/ItemPedido.java
@@ -1,12 +1,12 @@
 
 
-import pedido_livraria.produtos.Produto;
+import produtos.Produto;
 
 public class ItemPedido {
     private int quantidade;
     private Produto produto;
 
-    public ItemPedido(int quantidade, Produto produto) {
+    public ItemPedido( Produto produto, int quantidade) {
         this.quantidade = quantidade;
         this.produto = produto;
     }
