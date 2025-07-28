b06603447edb1f8fcbb940478c8329b3535af0b6
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-07-28 17:19:32 -0300

    exercício 4. Retângulo Completo

---
+++ Heranca/retangulo_completo/FormaGeometrica.java
@@ -0,0 +1,8 @@
+package retangulo_completo;
+
+public class FormaGeometrica {
+    public double area(){
+        throw  new UnsupportedOperationException("Funcao area nao implementada");
+
+    }
+}
