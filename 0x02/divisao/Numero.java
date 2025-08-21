7f58d94f358f3c2c31f56d4b2be0063025edd6cb
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-08-21 00:01:31 -0300

    ajuste exercicio Exceções (Parte I) 0. Divisão inteira

--- 0x02/divisao/Numero.java
+++ 0x02/divisao/Numero.java
@@ -2,14 +2,16 @@
 
 public class Numero {
     public static void dividir(int a, int b){
+        int resultado = 0;
         try {
-            int resultado = a / b;
+            resultado = a / b;
 
         } catch (Exception e) {
             System.out.println("Nao eh possivel dividir por zero");
 
         } finally {
-            System.out.println(a + " / " + b + " = " + a/b);
+
+            System.out.println(a + " / " + b + " = " + resultado);
         }
     }
 }
