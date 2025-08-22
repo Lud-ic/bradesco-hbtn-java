eabb1392461cd4ae99dc679987de53333d4c5386
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-08-22 20:39:06 -0300

    exercicio - File Handling 1. Leitura de Arquivos

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,23 @@
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadingExercise {
+    public static void main(String[] args) {
+        String fileName = "exemplo.txt";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            System.out.println("Conteúdo do arquivo '" + fileName + "':");
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            System.out.println("\nLeitura do arquivo concluída.");
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao ler o arquivo: " + e.getMessage());
+        }
+
+    }
+}
