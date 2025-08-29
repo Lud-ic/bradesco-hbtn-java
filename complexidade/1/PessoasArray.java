bd63474b5537e22c043e9e135bc1ffaade5ada44
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-08-28 21:26:34 -0300

    ajuste saída dados exercicio Complexidade de Algoritmos - 1. Complexidade O(n) – Busca Linear

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -22,13 +22,14 @@ public class PessoasArray {
         boolean encontrado = false;
         System.out.println("Procurando pelo nome: " + nome);
         for (int i = 0; i < nomes.length; i++) {
+            System.out.println("Passando pelo indice:" + i);
             if (nomes[i].equals(nome)) {
                 System.out.println("Nome pesquisado é " + nome + " que está na posição " + i);
                 encontrado = true;
-
-            } else {
-                System.out.println("Passando pelo indice:" + i);
             }
+        }
+        if (!encontrado) {
+            throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
         }
     }
 }
