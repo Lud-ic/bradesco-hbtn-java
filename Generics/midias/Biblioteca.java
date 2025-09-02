658f0d6e854188d6c1b5176ebddf53123052d542
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-09-01 22:49:36 -0300

    ajuste exercicio Generics 0. Mídias

--- Generics/midias/Biblioteca.java
+++ Generics/midias/Biblioteca.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class Biblioteca <T extends  Midia>{
     private List<T> midias = new ArrayList<>();
 
+    public Biblioteca() {
+        this.midias = new ArrayList<>();
+    }
+
     public void adicionarMidia(T midia) {
        midias.add(midia);
     }
