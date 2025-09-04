3bfcb7a6d4dfac3e09adc058bb354ae357ad70e1
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-09-04 19:16:05 -0300

    exercicio Collections List - 1. Cookies

--- List/cookies/Pedido.java
+++ List/cookies/Pedido.java
@@ -1,6 +1,40 @@
-package cookies;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Pedido {
-    public void adicionarPedidoCookie(PedidoCookie chocolate) {
+    ArrayList<PedidoCookie> cookies;
+
+    public Pedido() {
+        this.cookies = new ArrayList<>();
+    }
+
+    public void adicionarPedidoCookie(PedidoCookie pedidoCookie) {
+        this.cookies.add(pedidoCookie);
+    }
+
+
+    public int obterTotalCaixas() {
+        int totalCaixas = 0;
+        for (PedidoCookie pedidoCookie : this.cookies) {
+            totalCaixas += pedidoCookie.getQuantidadeCaixas();
+        }
+        return totalCaixas;
+    }
+
+
+    public int removerSabor(String sabor) {
+        int quantidadeCaixasRemovidas = 0;
+
+        Iterator<PedidoCookie> iterator = cookies.iterator();
+        while (iterator.hasNext()) {
+            PedidoCookie pedidoCookie = iterator.next();
+            if (pedidoCookie.getSabor().equals(sabor)) {
+                quantidadeCaixasRemovidas += pedidoCookie.getQuantidadeCaixas();
+                iterator.remove();
+            }
+        }
+        return quantidadeCaixasRemovidas;
     }
 }
--- List/cookies/PedidoCookie.java
+++ List/cookies/PedidoCookie.java
@@ -1,4 +1,19 @@
-package cookies;
+
 
 public class PedidoCookie {
+    String sabor;
+    int quantidadeCaixas;
+
+    public PedidoCookie(String sabor, int quantidadeCaixas) {
+        this.sabor = sabor;
+        this.quantidadeCaixas = quantidadeCaixas;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public int getQuantidadeCaixas() {
+        return quantidadeCaixas;
+    }
 }
