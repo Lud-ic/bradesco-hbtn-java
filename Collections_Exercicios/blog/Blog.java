38a65aae294210bdc3a6df420cad7b7eace474f6
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-09-04 22:01:12 -0300

    exercicio Collections - Exercícios de fixação - 0. Blog

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,32 @@
+
+
+import java.util.*;
+
+public class Blog {
+    List<Post> posts;
+
+    public Blog() {
+        this.posts = new ArrayList<>();
+    }
+
+    public void adicionarPostagem(Post post) {
+        posts.add(post);
+    }
+
+    public Set<String> obterTodosAutores() {
+        Set<String> autores = new TreeSet<>();
+        for (Post post : posts) {
+            autores.add(post.getAutor());
+        }
+        return autores;
+    }
+
+    public Map<String, Integer> obterContagemPorCategoria() {
+        Map<String, Integer> contagem = new TreeMap<>();
+        for (Post post : posts) {
+            String categoria = post.getCategoria();
+            contagem.put(categoria, contagem.getOrDefault(categoria, 0) + 1);
+        }
+        return contagem;
+    }
+}
