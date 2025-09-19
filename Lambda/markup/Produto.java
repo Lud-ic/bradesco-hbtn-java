4820495493e463eada2502eef29165615baf5827
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-09-18 21:02:08 -0300

    exercicio Lambda Expressions - 0. Produto Markup

---
+++ Lambda/markup/Produto.java
@@ -0,0 +1,61 @@
+package markup;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private double percentualMarkup = 10.0;
+    public Supplier<Double> precoComMarkup = () -> preco * (1 + percentualMarkup / 100);
+    public Consumer<Double> atualizarMarkup = novoPercentual -> percentualMarkup = novoPercentual;
+
+    public Produto(double preco, String nome) {
+        this.preco = preco;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getPercentualMarkup() {
+        return percentualMarkup;
+    }
+
+    public void setPercentualMarkup(double percentualMarkup) {
+        this.percentualMarkup = percentualMarkup;
+    }
+
+    public Supplier<Double> getPrecoComMarkup() {
+        return precoComMarkup;
+    }
+
+    public void setPrecoComMarkup(Supplier<Double> precoComMarkup) {
+        this.precoComMarkup = precoComMarkup;
+    }
+
+    public Consumer<Double> getAtualizarMarkup() {
+        return atualizarMarkup;
+    }
+
+    public void setAtualizarMarkup(Consumer<Double> atualizarMarkup) {
+        this.atualizarMarkup = atualizarMarkup;
+    }
+
+
+
+
+}
