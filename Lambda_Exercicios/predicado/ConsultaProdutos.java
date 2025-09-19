2bc83d3505b536cbc8aae4d86005823b2c5ce0c9
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-09-19 16:46:29 -0300

    ajuste Lambda Expressions - Exercícios de fixação - 1. Consultar Produtos com Predicado

--- Lambda_Exercicios/predicado/ConsultaProdutos.java
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class ConsultaProdutos {
-    static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> criterio) {
+    public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> criterio) {
         return produtos.stream().filter(criterio::test).toList();
     }
 }
