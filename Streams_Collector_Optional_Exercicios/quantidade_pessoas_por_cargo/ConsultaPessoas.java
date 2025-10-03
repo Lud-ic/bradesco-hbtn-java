0ed65538a95c1b8d18b1af2d89e3fbb22a79a215
Ludmila Magalhães <mag.ludmila@gmail.com>
2025-10-03 17:27:30 -0300

    exercicios Streams, Collectors e Optional - 1. Contagem de pessoas por cargo

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,23 @@
+package quantidade_pessoas_por_cargo;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        () -> new TreeMap<>(Comparator.reverseOrder()),
+                        Collectors.toCollection(TreeSet::new)
+                ));
+    }
+
+    static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        Collectors.counting()
+                ));
+    }
+}
